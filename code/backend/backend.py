97a70227c0a7fecb2404f19876a2ca39d1ad398b
franznkemaka <franz@rigle.co>
2024-02-06 14:20:24 +0100

    ✨ networks

--- code/backend/backend.py
+++ code/backend/backend.py
@@ -1,5 +1,6 @@
 from flask import Flask
-import socket, os
+import socket
+import os
 
 hostname = os.uname()[1]
 ips = socket.gethostbyname_ex(hostname)[2]
@@ -7,9 +8,10 @@ ips = socket.gethostbyname_ex(hostname)[2]
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return { "ip": str(ips), "username": "root" }
+    return {"ip": str(ips), "username": "root"}
+
 
 app.run(host='0.0.0.0', port=80, debug=True)
-
--- code/frontend/frontend.py
+++ code/frontend/frontend.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import socket
 import os
-# import requests
+import requests
 
 
 hostname = os.uname()[1]
@@ -14,11 +14,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-    # backend_response =  requests.get("http://backend:80").text
+    backend_response = requests.get("http://backend:80").text
     # backend_response = "Backend is coming later!"
     # backend_response = ips
-    return f'<html style="background:purple;">Hello World V4</html>'
-    # return f'<html style="background:orange;">Hello World V1, The backend said: {backend_response} </html>'
+    return f'<html style="background:orange;">Hello World V1, The backend said: {backend_response} </html>'
 
 
 app.run(host='0.0.0.0', port=80, debug=True)
