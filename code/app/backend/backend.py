bef503ff2596f7f221fd4b1cd5bc1810c8b7f9fd
franznkemaka <franz@rigle.co>
2024-02-09 12:22:05 +0100

    ✨  env & args

--- code/app/backend/backend.py
+++ code/app/backend/backend.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import socket
 import os
+from datetime import datetime
 
 hostname = os.uname()[1]
 ips = socket.gethostbyname_ex(hostname)[2]
@@ -11,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return {"ip": str(ips), "username": "root"}
+    return {"ip": str(ips), "username": "root", "now": datetime.now()}
 
 
 app.run(host='0.0.0.0', port=80, debug=True)
