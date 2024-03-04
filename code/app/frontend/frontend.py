eb15cb6d7fa3d657fba366ec571d3154f2172e74
franznkemaka <franz@rigle.co>
2024-03-04 11:13:36 +0100

    ✨ k8s 19.02

--- code/app/frontend/frontend.py
+++ code/app/frontend/frontend.py
@@ -9,7 +9,8 @@ ips = socket.gethostbyname_ex(hostname)[2]
 
 # port = os.getenv("FRONTEND_PORT")
 port = 5000
-backend_url = os.getenv("BACKEND_URL")
+backend_url = "http://backend:80"
+# backend_url = os.getenv("BACKEND_URL")
 
 
 app = Flask(__name__)
@@ -17,12 +18,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # backend_response = requests.get(backend_url).text
+    backend_response = requests.get(backend_url).text
 
     # backend_response = requests.get("http://backend:80").text
     # backend_response = "Backend is coming later!"
-    backend_response = ips
-    return f'<html style="background:green;">Hello World V1, The backend said: {backend_response} </html>'
+    # backend_response = ips
+    return f'<html style="background:orange;">Hello World V2, The backend said: {backend_response} </html>'
     # return f'<html style="background:green;">Hello World V1</html>'
 
 
