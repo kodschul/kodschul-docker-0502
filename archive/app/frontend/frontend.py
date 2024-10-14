d52156ca0cb96b0b75049048536503d33cdd7bd1
franznkemaka <franz@rigle.co>
2024-10-14 14:02:18 +0200

    ✨

--- archive/app/frontend/frontend.py
+++ archive/app/frontend/frontend.py
@@ -7,23 +7,17 @@ import requests
 hostname = os.uname()[1]
 ips = socket.gethostbyname_ex(hostname)[2]
 
-# port = os.getenv("FRONTEND_PORT")
-port = 5000
-backend_url = "http://backend:80"
-# backend_url = os.getenv("BACKEND_URL")
-
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    backend_response = requests.get(backend_url).text
 
-    # backend_response = requests.get("http://backend:80").text
+    backend_response = requests.get("http://backend:80").text
     # backend_response = "Backend is coming later!"
     # backend_response = ips
-    return f'<html style="background:orange;">Hello World V2, The backend said: {backend_response} </html>'
+    return f'<html style="background:green;">Hello World V2, IP: {ips}, Backend Response: {backend_response} </html>'
     # return f'<html style="background:green;">Hello World V1</html>'
 
 
