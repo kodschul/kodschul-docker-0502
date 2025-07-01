757ad2457f5adbad92027bd518042c6c47ac88d2
franznkemaka <info@franznkemaka.com>
2025-07-01 12:10:19 +0200

    🚀

--- 0107/frontend/frontend.py
+++ 0107/frontend/frontend.py
@@ -13,7 +13,7 @@ ips = socket.gethostbyname_ex(hostname)[2]
 # port = os.getenv("FRONTEND_PORT")
 # port = 5000
 # backend_url = "http://backend:80"
-# backend_url = os.getenv("BACKEND_URL")
+backend_url = os.getenv("BACKEND_URL")
 
 
 app = Flask(__name__)
@@ -22,16 +22,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-    os.makedirs("/data", exist_ok=True)
-    with open("/data/access.log", "a+") as f:
-        f.write(f"{datetime.now()}: IP: {ips} \n")
+    # os.makedirs("/data", exist_ok=True)
+    # with open("/data/access.log", "a+") as f:
+    #     f.write(f"{datetime.now()}: IP: {ips} \n")
 
     # backend_response = requests.get(backend_url).text
 
     # backend_response = requests.get("http://backend:80").text
     backend_response = "Backend is coming later!"
     # backend_response = ips
-    return f'<html style="background:gray;">Hello World V4, IP: {ips}, Backend Response: {backend_response} </html>'
+    return f'<html style="background:yellow;">Hello World V1, IP: {ips}, Backend Response: {backend_response} </html>'
 
 
 app.run(host='0.0.0.0', port=80, debug=True)
