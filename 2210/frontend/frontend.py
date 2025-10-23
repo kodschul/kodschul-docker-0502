559049d404b6e3d50aec97c3e8e0ba28f5a51640
franznkemaka <info@franznkemaka.com>
2025-10-23 13:40:12 +0200

    🚀

--- 2210/frontend/frontend.py
+++ 2210/frontend/frontend.py
@@ -28,8 +28,8 @@ def index():
 
     # backend_response = requests.get(backend_url).text
 
-    # backend_response = requests.get("http://backend:80").text
-    backend_response = "Backend is coming later!"
+    backend_response = requests.get("http://backend:80").text
+    # backend_response = "Backend is coming later!"
     # backend_response = ips
     return f'<html style="background:orange;">Hello World V1, IP: {ips}, Backend Response: {backend_response} </html>'
 
